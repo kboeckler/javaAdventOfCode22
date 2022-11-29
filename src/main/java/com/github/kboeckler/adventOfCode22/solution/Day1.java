28b6adc3fb29d2d52ce8c216e463250e062802bd
Kevin Böckler <kevinboeckler@gmail.com>
2022-11-29 21:36:59 +0100

    Add runner framework

---
+++ src/main/java/com/github/kboeckler/adventOfCode22/solution/Day1.java
@@ -0,0 +1,22 @@
+package com.github.kboeckler.adventOfCode22.solution;
+
+import com.github.kboeckler.adventOfCode22.Runner;
+
+import java.io.Serializable;
+
+public class Day1 implements Solution{
+
+    static {
+        Runner.registerSolution(1, new Day1());
+    }
+
+    @Override
+    public Serializable solvePart1(String[] input) {
+        return null;
+    }
+
+    @Override
+    public Serializable solvePart2(String[] input) {
+        return null;
+    }
+}
---
+++ src/main/java/com/github/kboeckler/adventOfCode22/solution/Solution.java
@@ -0,0 +1,11 @@
+package com.github.kboeckler.adventOfCode22.solution;
+
+import java.io.Serializable;
+
+public interface Solution {
+
+    Serializable solvePart1(String[] input);
+
+    Serializable solvePart2(String[] input);
+
+}
