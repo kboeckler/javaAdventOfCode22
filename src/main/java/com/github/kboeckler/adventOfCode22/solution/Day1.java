66c030c290a1eaea36083f335ee455ccfb95ac16
Kevin Böckler <kevinboeckler@gmail.com>
2022-12-01 07:13:16 +0100

    Day 1

--- src/main/java/com/github/kboeckler/adventOfCode22/solution/Day1.java
+++ src/main/java/com/github/kboeckler/adventOfCode22/solution/Day1.java
@@ -3,20 +3,43 @@ package com.github.kboeckler.adventOfCode22.solution;
 import com.github.kboeckler.adventOfCode22.Runner;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Day1 implements Solution{
+public class Day1 implements Solution {
 
-    static {
-        Runner.registerSolution(1, new Day1());
+  static {
+    Runner.registerSolution(1, new Day1());
+  }
+
+  @Override
+  public Serializable solvePart1(String[] input) {
+    return parseElfChunks(input).stream()
+        .map(elf -> elf.stream().map(Integer::parseInt).reduce(0, Integer::sum))
+        .max(Integer::compareTo)
+        .orElse(0);
+  }
+
+  @Override
+  public Serializable solvePart2(String[] input) {
+    return parseElfChunks(input).stream()
+        .map(elf -> elf.stream().map(Integer::parseInt).reduce(0, Integer::sum))
+        .sorted((a, b) -> Integer.compare(b, a))
+        .limit(3)
+        .reduce(0, Integer::sum);
+  }
+
+  private List<List<String>> parseElfChunks(String[] input) {
+    List<List<String>> elves = new ArrayList<>();
+    List<String> currentElf = new ArrayList<>();
+    for (String row : input) {
+      if (row.isBlank()) {
+        elves.add(currentElf);
+        currentElf = new ArrayList<>();
+        continue;
+      }
+      currentElf.add(row);
     }
-
-    @Override
-    public Serializable solvePart1(String[] input) {
-        return null;
-    }
-
-    @Override
-    public Serializable solvePart2(String[] input) {
-        return null;
-    }
+    return elves;
+  }
 }
