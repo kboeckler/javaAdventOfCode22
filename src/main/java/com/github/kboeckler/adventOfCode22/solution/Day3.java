5d21cbafd742a5557285dda65959f1cbbaf90c55
Kevin Böckler <kevinboeckler@gmail.com>
2022-12-03 18:56:38 +0100

    Day 3

---
+++ src/main/java/com/github/kboeckler/adventOfCode22/solution/Day3.java
@@ -0,0 +1,63 @@
+package com.github.kboeckler.adventOfCode22.solution;
+
+import com.github.kboeckler.adventOfCode22.Runner;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day3 implements Solution {
+
+  static {
+    Runner.registerSolution(3, new Day3());
+  }
+
+  @Override
+  public Serializable solvePart1(String[] input) {
+    List<List<String>> groupsOfParts = new ArrayList<>();
+    for (String row : input) {
+      List<String> parts = new ArrayList<>();
+      parts.add(row.substring(0, row.length() / 2));
+      parts.add(row.substring(row.length() / 2));
+      groupsOfParts.add(parts);
+    }
+    return groupsOfParts.stream()
+        .map(this::toSharedString)
+        .map(this::stringToPriority)
+        .reduce(0, Integer::sum);
+  }
+
+  @Override
+  public Serializable solvePart2(String[] input) {
+    List<List<String>> groupsOfParts = new ArrayList<>();
+    for (int i = 0; i < input.length / 3; i++) {
+      List<String> parts = new ArrayList<>();
+      parts.add(input[i * 3]);
+      parts.add(input[(i * 3) + 1]);
+      parts.add(input[(i * 3) + 2]);
+      groupsOfParts.add(parts);
+    }
+    return groupsOfParts.stream()
+        .map(this::toSharedString)
+        .map(this::stringToPriority)
+        .reduce(0, Integer::sum);
+  }
+
+  private String toSharedString(List<String> parts) {
+    return parts.stream()
+        .reduce(
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            (a, b) -> {
+              String nonCommonLetters = a.replaceAll("[" + b + "]", "");
+              return a.replaceAll("[" + nonCommonLetters + "]", "");
+            });
+  }
+
+  private int stringToPriority(String str) {
+    return str.chars().distinct().map(this::toPriority).sum();
+  }
+
+  private int toPriority(int letter) {
+    return letter >= (int) 'a' ? letter - (int) 'a' + 1 : letter - (int) 'A' + 27;
+  }
+}
